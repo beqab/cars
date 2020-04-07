45bd4cb8ce25ace3c596d52b1c1f45b027707a4c
beqa <badagadzeb@gmail.com>
2020-04-07 23:33:09 +0400

    add login validation

--- components/regAuth/Auth/Authorizhation.js
+++ components/regAuth/Auth/Authorizhation.js
@@ -1,34 +1,74 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Link } from "react-dom";
 import JVTdecode from "jwt-decode";
+import { loginValidation } from "../../validator/validation";
+import classname from "classnames";
 class Authorization extends Component {
   state = {
     email: "",
     password: "",
+    errors: [],
+    validated: false,
   };
 
   inputHandler = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
+    this.setState(
+      {
+        [e.target.name]: e.target.value,
+      },
+      () => {
+        if (this.state.validated) {
+          let { errors, isValid } = loginValidation({
+            email: this.state.email,
+            password: this.state.password,
+          });
+          if (isValid) {
+            this.setState({
+              errors: [],
+            });
+          } else {
+            this.setState({
+              errors: errors,
+            });
+          }
+        }
+      }
+    );
   };
 
   hangleLogIn = (e) => {
     e.preventDefault();
 
-    axios
-      .post("http://localhost:5000/api/auth", {
-        email: this.state.email,
-        password: this.state.password,
-      })
-      .then((res) => {
-        let test = JVTdecode(res.data.token);
-        console.log(test);
-      })
-      .catch((err) => {
-        console.log(err);
+    let { errors, isValid } = loginValidation({
+      email: this.state.email,
+      password: this.state.password,
+    });
+    if (isValid) {
+      this.setState(
+        {
+          errors: [],
+        },
+        () => {
+          axios
+            .post("http://localhost:5000/api/auth", {
+              email: this.state.email,
+              password: this.state.password,
+            })
+            .then((res) => {
+              let test = JVTdecode(res.data.token);
+              console.log(test);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      );
+    } else {
+      this.setState({
+        errors: errors,
+        validated: true,
       });
+    }
   };
   render() {
     return (
@@ -47,7 +87,13 @@ class Authorization extends Component {
                   name="email"
                   type="mail"
                   placeholder="YourEmail@gmail.com"
+                  className={classname("form-control", {
+                    "is-invalid": this.state.errors.email,
+                  })}
                 />
+                <span className="invalid-feedback">
+                  {this.state.errors.email}
+                </span>
               </div>
               <div className="input_box">
                 <label htmlFor="password">პაროლი</label>
@@ -56,7 +102,13 @@ class Authorization extends Component {
                   name="password"
                   type="password"
                   placeholder="* * * * * * *"
+                  className={classname("form-control", {
+                    "is-invalid": this.state.errors.password,
+                  })}
                 />
+                <span className="invalid-feedback">
+                  {this.state.errors.password}
+                </span>
               </div>
 
               <div id="login_box">
