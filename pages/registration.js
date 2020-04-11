f2cf5265b9d58b0c2e7164573bee83cf4fb1fe5a
tornikezhizhi <zhizhiashvilitornike@gmail.com>
2020-04-11 23:49:07 +0400

    add regComponent.js wihouth onChange validations

---
+++ pages/registration.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../style/style.scss";
+// import "../style/bootstrap.scss";
+import BaceLayout from "../components/baceLayout/baceLayout";
+import Registration from "../components/Registration/regComponent";
+const Registr = () => (
+  <BaceLayout>
+    <Registration></Registration>
+    {/* <Home></Home> */}
+  </BaceLayout>
+);
+//
+
+export default Registr;
