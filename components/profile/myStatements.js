1eec72a25958bb7215c1abfb161cd1f392752ba1
beqa <badagadzeb@gmail.com>
2020-04-18 20:24:02 +0400

    add all users statements

--- components/profile/myStatements.js
+++ components/profile/myStatements.js
@@ -1,24 +1,36 @@
 import React, { Component } from "react";
 import Cookies from "js-cookie";
+import axios from "axios";
 
 class myStatements extends Component {
+  state = {
+    myStatements: [],
+  };
+
   componentDidMount() {
     let token = Cookies.get("token");
-    // axios
-    //   .get("statement/sum", {
-    //     headers: {
-    //       Authorization: `Bearer ${token}`,
-    //     },
-    //   })
-    //   .then((res) => {
-    //     console.log(res);
-    //     this.setState({
-    //       statementSum: res.data,
-    //     });
-    //   });
+    axios
+      .get("statement/myall", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          myStatements: res.data,
+        });
+      });
   }
+
   render() {
-    return <div>gancxadebebi:</div>;
+    return (
+      <div>
+        {this.state.myStatements.map((el) => {
+          return <h1>my stamant</h1>;
+        })}
+      </div>
+    );
   }
 }
 
