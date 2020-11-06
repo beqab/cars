b618bbb18c329f4d90d65c6c36cbcd00fceac795
unknown <badagadzeb@gmail.com>
2020-11-06 20:55:38 +0400

    login refactor

--- components/regAuth/Auth/Authorizhation.js
+++ components/regAuth/Auth/Authorizhation.js
@@ -1,195 +1,131 @@
-import React, { Component } from "react";
+import React, { useState, useCallback } from "react";
 import axios from "axios";
-import JVTdecode from "jwt-decode";
-import { loginValidation } from "../../validator/validation";
-import classname from "classnames";
-import { connect } from "react-redux";
-import { setCurrentUser } from "../../../redux/auth/authActions";
+import { useDispatch } from "react-redux";
 import Link from "next/link";
+import classnames from "classnames";
+import validator from "validator";
+import { useForm } from "react-hook-form";
+
+import { setCurrentUser } from "../../../redux/auth/authActions";
 import FBLogin from "./Fblogin";
 
-class Authorization extends Component {
-  state = {
-    email: "",
-    password: "",
-    errors: [],
-    validated: false,
-    butDisable: false,
-    errorMsgs: null,
-  };
+const Authorization = () => {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+  const [serverError, setServerError] = useState([]);
 
-  inputHandler = (e) => {
-    this.setState(
-      {
-        [e.target.name]: e.target.value,
-        errorMsgs: null,
-      },
-      () => {
-        if (this.state.validated) {
-          let { errors, isValid } = loginValidation({
-            email: this.state.email,
-            password: this.state.password,
-          });
-          if (isValid) {
-            this.setState({
-              errors: [],
-            });
-          } else {
-            this.setState({
-              errors: errors,
-            });
-          }
-        }
-      }
-    );
-  };
+  const { handleSubmit, errors, register } = useForm();
 
-  hangleLogIn = (e) => {
-    e.preventDefault();
+  const handleLogIn = useCallback(async (data) => {
+    setLoading(true);
 
-    let { errors, isValid } = loginValidation({
-      email: this.state.email,
-      password: this.state.password,
-    });
-    if (isValid) {
-      this.setState(
-        {
-          errors: [],
-          butDisable: true,
-        },
-        () => {
-          axios
-            .post("auth", {
-              email: this.state.email,
-              password: this.state.password,
-            })
-            .then((res) => {
-              let test = JVTdecode(res.data.token);
-              console.log(test);
-              this.setState({
-                errors: [],
-                butDisable: false,
-              });
-              this.props.setCurrentUser({
-                user: res.data.user,
-                token: res.data.token,
-              });
-              // this.props.setCurrentUser({
-              //   user: res.data.user,
-              //   token: res.data.token
-              // });
-            })
-            .catch((err) => {
-              console.log(err, "errrr");
+    try {
+      const res = await axios.post("auth", data);
 
-              if (err.response.status > 400) {
-                this.setState({
-                  butDisable: false,
-                  errorMsgs: "კავშირის პრობლემა :/ სცადეთ თავიდან",
-                });
-              } else {
-                this.setState({
-                  butDisable: false,
-                  errorMsgs: err.response.data.errors,
-                });
-              }
-              // if (err.response.status != 500) {
-              //   this.setState({
-              //     loginerrors: { email: "მეილი ან პარალლი არაწორია" },
-              //   });
-              // }
-            });
-        }
+      setLoading(false);
+      dispatch(
+        setCurrentUser({
+          user: res.data.user,
+          token: res.data.token,
+        })
       );
-    } else {
-      this.setState({
-        errors: errors,
-        validated: true,
-      });
+    } catch (err) {
+      if (err.response.status > 400) {
+        setServerError("კავშირის პრობლემა :/ სცადეთ მოგვიანებით");
+      } else {
+        setServerError(err.response.data.errors);
+      }
+      setLoading(false);
     }
-  };
-  render() {
-    return (
-      <div id="authorizathion">
-        <div className="authorization_box">
-          <div className="auth_title">
-            <h3>ავტორიზაცია</h3>
+  }, []);
+
+  const inputHandler = useCallback(() => {
+    setServerError(null);
+  }, []);
+
+  return (
+    <div id="authorizathion">
+      <div className="authorization_box">
+        <div className="auth_title">
+          <h3>ავტორიზაცია</h3>
+        </div>
+
+        <form onSubmit={handleSubmit(handleLogIn)} id="auth_form">
+          {serverError && <div style={{ color: "red" }}>{serverError}</div>}
+
+          <div className="auth_inputs">
+            <div className="input_box">
+              <label htmlFor="email">მეილი</label>
+              <input
+                ref={register({
+                  required: "მეილი აუცილებელია",
+                  validate: {
+                    isEmail: (value) => validator.isEmail(value),
+                  },
+                })}
+                onChange={inputHandler}
+                name="email"
+                type="mail"
+                placeholder="YourEmail@gmail.com"
+                className={classnames("form-control", {
+                  "is-invalid": errors.email,
+                })}
+              />
+              <span className="invalid-feedback">
+                {errors.email && errors.email.type === "isEmail"
+                  ? "მეილი არასწორი ფორმატისაა"
+                  : "მეილი აცილებელია"}
+              </span>
+            </div>
+            <div className="input_box">
+              <label htmlFor="password">პაროლი</label>
+              <input
+                onChange={inputHandler}
+                ref={register({
+                  required: true,
+                })}
+                name="password"
+                type="password"
+                placeholder="* * * * * * *"
+                className={classnames("form-control", {
+                  "is-invalid": errors.password,
+                })}
+              />
+              <span className="invalid-feedback">{"პაროლი აუცილებელია"}</span>
+            </div>
+            <div
+              className={classnames("loader_box", {
+                hide: !loading,
+              })}
+            >
+              <div className="loader" id="loader-1"></div>
+            </div>
+            <div id="login_box">
+              <button className="load" disabled={loading} id="login_button">
+                შესვლა
+              </button>
+            </div>
+          </div>
+          <div className="social_auth">
+            <a href="" className="google_auth_social">
+              <img src="/imgs/google.png" alt="" />
+              <span>by Google</span>
+            </a>
+            <FBLogin />
           </div>
 
-          <form onSubmit={this.hangleLogIn} id="auth_form">
-            {this.state.errorMsgs && (
-              <div style={{ color: "red" }}>{this.state.errorMsgs}</div>
-            )}
-
-            <div className="auth_inputs">
-              <div className="input_box">
-                <label htmlFor="email">მეილი</label>
-                <input
-                  onChange={this.inputHandler}
-                  name="email"
-                  type="mail"
-                  placeholder="YourEmail@gmail.com"
-                  className={classname("form-control", {
-                    "is-invalid": this.state.errors.email,
-                  })}
-                />
-                <span className="invalid-feedback">
-                  {this.state.errors.email}
-                </span>
-              </div>
-              <div className="input_box">
-                <label htmlFor="password">პაროლი</label>
-                <input
-                  onChange={this.inputHandler}
-                  name="password"
-                  type="password"
-                  placeholder="* * * * * * *"
-                  className={classname("form-control", {
-                    "is-invalid": this.state.errors.password,
-                  })}
-                />
-                <span className="invalid-feedback">
-                  {this.state.errors.password}
-                </span>
-              </div>
-              <div
-                className={classname("loader_box", {
-                  hide: !this.state.butDisable,
-                })}
-              >
-                <div className="loader" id="loader-1"></div>
-              </div>
-              <div id="login_box">
-                <button
-                  className="load"
-                  disabled={this.state.butDisable}
-                  id="login_button"
-                >
-                  შესვლა
-                </button>
-              </div>
-            </div>
-            <div className="social_auth">
-              <a href="" className="google_auth_social">
-                <img src="/imgs/google.png" alt="" />
-                <span>by Google</span>
-              </a>
-              <FBLogin />
-            </div>
-
-            <div className="auth_footer">
-              <Link href="/registration">
-                <a>დარეგისტრირდი</a>
-              </Link>
-              <Link href="/roceverpass">
-                <a>დაგავიწყდათ პაროლი?</a>
-              </Link>
-            </div>
-          </form>
-        </div>
+          <div className="auth_footer">
+            <Link href="/registration">
+              <a>დარეგისტრირდი</a>
+            </Link>
+            <Link href="/roceverpass">
+              <a>დაგავიწყდათ პაროლი?</a>
+            </Link>
+          </div>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default connect(null, { setCurrentUser })(Authorization);
+export default Authorization;
--- components/validator/validation.js
+++ components/validator/validation.js
@@ -1,21 +1,6 @@
 import validator from "validator";
 import isEmpty from "./isEmpty";
 
-export const loginValidation = (data) => {
-  let errors = {};
-  if (!validator.isEmail(data.email)) {
-    errors.email = "ელ. ფოსტა სწორი ფორმატით";
-  }
-  if (!validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = "პაროლი მინიმუმ 6 სიმბოლო";
-  }
-
-  return {
-    errors,
-    isValid: isEmpty(errors),
-  };
-};
-
 export const regValidation = (data) => {
   let errors = {};
   if (!validator.isEmail(data.email)) {
