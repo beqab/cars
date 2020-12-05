cbde5f495c1ae6293cbc91843b270a7d4a93bcc3
unknown <badagadzeb@gmail.com>
2020-12-05 23:03:59 +0400

    ff

--- components/search/search.js
+++ components/search/search.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 import JVTdecode from "jwt-decode";
-import { regValidation } from "../validator/validation";
 import classname from "classnames";
 import { connect } from "react-redux";
 import { setCurrentUser } from "../../redux/auth/authActions";
