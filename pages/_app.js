082ac9ca91881189e4e9812c1cb4bab8d00d79a3
unknown <badagadzeb@gmail.com>
2020-12-30 03:37:29 +0400

    ff

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,6 @@ import { getCookesFromReq } from "../helpers/utils";
 import Cookies from "js-cookie";
 import axios from "axios";
 import { hotjar } from "react-hotjar";
-hotjar.initialize(2175859, 6);
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000/api/"
@@ -41,26 +40,14 @@ class MyApp extends App {
   }
 
   async componentDidMount() {
+    hotjar.initialize(2175859, 6);
+
     // console.log(process.env.NODE_ENV, "process.env.NODE_ENV ");
     // debugger;
     let token = Cookies.get("token");
     if (token) {
       axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
     }
-
-    // (function (h, o, t, j, a, r) {
-    //   h.hj =
-    //     h.hj ||
-    //     function () {
-    //       (h.hj.q = h.hj.q || []).push(arguments);
-    //     };
-    //   h._hjSettings = { hjid: 2175859, hjsv: 6 };
-    //   a = o.getElementsByTagName("head")[0];
-    //   r = o.createElement("script");
-    //   r.async = 1;
-    //   r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-    //   a.appendChild(r);
-    // })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
   }
   render() {
     //pageProps that were returned  from 'getInitialProps' are stored in the props i.e. pageprops
