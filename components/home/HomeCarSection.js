7bce320956cf6370b0dc2b018a5dd1de5a1c16d4
unknown <badagadzeb@gmail.com>
2021-01-30 23:27:19 +0400

    add loaders fix menu on mobile serch bar sxrooll

--- components/home/HomeCarSection.js
+++ components/home/HomeCarSection.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Link from "next/link";
 import axios from "axios";
 import Card from "./card";
-
+import Loader from "../UI/loader";
 class HomeCar extends Component {
   state = {
     statements: [],
@@ -28,13 +28,23 @@ class HomeCar extends Component {
       <div className="home-car_section">
         <div className="container">
           <div className="row">
-            {this.state.statements.map((el, i) => {
-              return (
-                <div className="col-12 col-md-3">
-                  <Card data={el} />
-                </div>
-              );
-            })}
+            {this.state.statements && !this.state.statements.length ? (
+              this.state.statements.map((el, i) => {
+                return (
+                  <div className="col-12 col-md-3">
+                    <Card data={el} />
+                  </div>
+                );
+              })
+            ) : (
+              <div
+                style={{ minHeight: "70vh" }}
+                className="text-center w-100 d-flex justify-content-center align-items-center flex-column"
+              >
+                <img src="https://gifprint.s3.amazonaws.com/p/gif/80037/e62c3f40a25701360b561e9875f84da0.gif" />
+                <div>იტვირთება...</div>
+              </div>
+            )}
           </div>
 
           <div className="row">
